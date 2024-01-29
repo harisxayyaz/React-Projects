4b9e5d650f26a8f018173da2560e747b383c7b8f
harisxayyaz <harisayyaz42@gmail.com>
2024-01-29 16:55:40 +0500

    Contact app using Firebase and Tailwind CSS

---
+++ project-5/src/App.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const App = () => {
+  return <div className="bg-blue-500 flex">App</div>;
+};
+
+export default App;
---
+++ project-5/src/config/firebase.js
@@ -0,0 +1,20 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+
+import { getFireStore } from "firebase/firestore";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBaBL33OqJ3kArW9w4knI_T8kSic64cJjQ",
+  authDomain: "vite-contact-52ee6.firebaseapp.com",
+  projectId: "vite-contact-52ee6",
+  storageBucket: "vite-contact-52ee6.appspot.com",
+  messagingSenderId: "133317868489",
+  appId: "1:133317868489:web:ec1eec4d7878e4b2b4c618",
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
+export const dp = getFireStore(app);
