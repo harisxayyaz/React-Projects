1826d226bf2e295d10dd1680dc113c198d20c049
harisxayyaz <harisayyaz42@gmail.com>
2024-01-30 22:54:04 +0500

    Added Navbar Component

---
+++ project-5/src/components/Navbar.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <div className="m-2  w-[361px] h-[60px] bg-white flex items-center justify-center gap-2 rounded">
+      <img src="./logos_firebase.svg" alt="firebaselogo" />
+      Firebase Contact App
+    </div>
+  );
+};
+
+export default Navbar;
